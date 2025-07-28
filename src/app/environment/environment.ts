150d4826e840fd346782057a42b674d7031a5c81
PujaSah-max <psah@ctepl.com>
2025-07-28 19:34:38 +0530

    chore: added azure backend url

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
 //   production: false,
-  apiBaseUrl: 'http://localhost:5048/api/Resource/',
+  // apiBaseUrl: 'http://localhost:5048/api/Resource/',
+  apiBaseUrl: 'https://resourcetrackerwebapp-cvhje7faeudmeedx.canadacentral-01.azurewebsites.net/api/Resource/'
 };
