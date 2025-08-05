9969deef26782c7abe2384eda22abe8eeb964e6e
PujaSah-max <psah@ctepl.com>
2025-08-06 00:09:53 +0530

    chore: dropdown changes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -5,18 +5,19 @@ import { ServiceService } from '../services/resourceService';
 import { ActivatedRoute, Router, } from '@angular/router';
 import { isVisible } from '@progress/kendo-angular-common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-
+import { DropDownsModule, MultiSelectModule } from '@progress/kendo-angular-dropdowns';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { techSkills } from '../Constants/constants';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, FormsModule, HttpClientModule, DropDownsModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule, HttpClientModule, DropDownsModule, MultiSelectModule, NgSelectModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
-  constructor(private service: ServiceService, private activatedroute: ActivatedRoute, private router: Router,http: HttpClient) { }
+  constructor(private service: ServiceService, private activatedroute: ActivatedRoute, private router: Router, http: HttpClient) { }
   public isEditMode: boolean = false;
   myform: FormGroup = new FormGroup({
     empId: new FormControl("EMP", [Validators.required, Validators.minLength(3)]),
@@ -24,7 +25,7 @@ export class FormComponent {
     designation: new FormControl("", [Validators.required, Validators.minLength(3)]),
     reporting_To: new FormControl("", [Validators.required, Validators.minLength(3)]),
     billable: new FormControl("", [Validators.required]),
-    skills: new FormControl("", [Validators.required]),
+    skills: new FormControl([], [Validators.required]),
     project_Allocation: new FormControl("", [Validators.required]),
     location: new FormControl("", [Validators.required]),
     otherLocation: new FormControl(""),
@@ -39,6 +40,7 @@ export class FormComponent {
   public skillsList: string[] = [];
   projectList: string[] = [];
 
+  techSkills: any[] = techSkills;
 
 
   empId!: string;
@@ -49,9 +51,9 @@ export class FormComponent {
       this.newDetails = data;
       this.designations = [...new Set(this.newDetails.map(item => item.designation))];
       this.skillsList = [...new Set(this.newDetails.map(item => item.skills))];
-       this.projectList = [...new Set(this.newDetails.map(item => item.project_Allocation))];
+      this.projectList = [...new Set(this.newDetails.map(item => item.project_Allocation))];
     });
-    
+
     if (this.empId)
       this.service.GetEmployeeById(this.empId).subscribe((data: any) => {
         console.log("data", data);
@@ -63,6 +65,10 @@ export class FormComponent {
         }
         const billableValue = data.billable ? 'YES' : 'NO';
 
+        data.skills = data.skills.split(',').map((skill: string) => {
+          return skill.trim();
+        });
+
         this.myform.patchValue({
           ...data,
           billable: billableValue
@@ -70,25 +76,25 @@ export class FormComponent {
 
 
       })
-    
+
     if (this.empId) {
-      this.isEditMode = true;  
+      this.isEditMode = true;
     } else {
-    this.isEditMode = false;
+      this.isEditMode = false;
     }
   }
 
   onDesignationChange(value: string) {
-  this.myform.get('designation')?.setValue(value);
-}
+    this.myform.get('designation')?.setValue(value);
+  }
 
-onSkillChange(value: string): void {
-  this.myform.get('skills')?.setValue(value);
-}
+  onSkillChange(value: string): void {
+    this.myform.get('skills')?.setValue(value);
+  }
 
-onProjectChange(value: string): void {
-  this.myform.get('project_Allocation')?.setValue(value);
-}
+  onProjectChange(value: string): void {
+    this.myform.get('project_Allocation')?.setValue(value);
+  }
 
   submitted = false;
 
@@ -109,6 +115,7 @@ onProjectChange(value: string): void {
   ];
   Onsubmit() {
     console.log(this.myform.value);
+    console.log("Payload sent to API:", this.myform.value);
 
 
     let obj = {
@@ -117,11 +124,11 @@ onProjectChange(value: string): void {
       Designation: this.myform.get('Designation')?.value,
       Reporting_To: this.myform.get('Reporting_To')?.value,
       billable: this.myform.get('billable')?.value === "YES" ? true : false,
-      Skills: this.myform.get('Skills')?.value,
+      skills: this.myform.get('skills')?.value,
       Project_Allocation: this.myform.get('Project_Allocation')?.value,
       location: this.myform.get('location')?.value === 'Other'
-      ? this.myform.get('OtherLocation')?.value
-      : this.myform.get('location')?.value,
+        ? this.myform.get('OtherLocation')?.value
+        : this.myform.get('location')?.value,
       OtherLocation: this.myform.get('otherlocation')?.value,
       Email: this.myform.get('Email')?.value,
       CTE_DOJ: (this.myform.get('CTE_DOJ')?.value),
@@ -141,25 +148,27 @@ onProjectChange(value: string): void {
       data.location = data.location === "Other" ? this.myform.get("otherLocation")?.value
         : this.myform.get("location")?.value;
 
+      data.skills = data.skills.join(', ');
+
       console.log("data", data);
       this.service.updateempdetails(data).subscribe((response) => {
         this.router.navigate(['/home']);
         const data = this.myform.value;
 
-    alert(
-      `Employee Details Updated:\n\n` +
-      `EmpID: ${data.empId}\n` +
-      `Name: ${data.name}\n` +
-      `Designation: ${data.designation}\n` +
-      `Reporting_To: ${data.reportingTo}\n` +
-      `Billable: ${data.billableStatus}\n` +
-      `Skills: ${data.skills}\n` +
-      `Project_Allocation: ${data.projectAllocation}\n` +
-      `Location: ${data.location}\n` +
-      `Email: ${data.email}\n` +
-      `CTE_DOJ: ${data.cteDoj}\n` +
-      `Remarks: ${data.remarks}`
-    );
+        alert(
+          `Employee Details Updated:\n\n` +
+          `EmpID: ${data.empId}\n` +
+          `Name: ${data.name}\n` +
+          `Designation: ${data.designation}\n` +
+          `Reporting_To: ${data.reportingTo}\n` +
+          `Billable: ${data.billableStatus}\n` +
+          `Skills: ${data.skills}\n` +
+          `Project_Allocation: ${data.projectAllocation}\n` +
+          `Location: ${data.location}\n` +
+          `Email: ${data.email}\n` +
+          `CTE_DOJ: ${data.cteDoj}\n` +
+          `Remarks: ${data.remarks}`
+        );
         this.myform.reset();
       });
     } else {
@@ -167,29 +176,31 @@ onProjectChange(value: string): void {
       data.billable = data.billable == "YES" ? true : false;
       data.location = data.location === "Other" ? this.myform.get("otherLocation")?.value
         : this.myform.get("location")?.value;
+      let skillsArray: string[] = data.skills;
+      data.skills = skillsArray.join(', ');
 
       console.log("Inside Add New MEployee ", data);
       this.service.Addempdetails(data).subscribe((response) => {
         console.log(this.myform.value)
         this.router.navigate(['/home']);
 
-        
+
         const data = this.myform.value;
 
-    alert(
-      `Employee Details Submitted:\n\n` +
-      `EmpID: ${data.empId}\n` +
-      `Name: ${data.name}\n` +
-      `Designation: ${data.designation}\n` +
-      `Reporting_To: ${data.reportingTo}\n` +
-      `Billable: ${data.billableStatus}\n` +
-      `Skills: ${data.skills}\n` +
-      `Project_Allocation: ${data.projectAllocation}\n` +
-      `Location: ${data.location}\n` +
-      `Email: ${data.email}\n` +
-      `CTE_DOJ: ${data.cteDoj}\n` +
-      `Remarks: ${data.remarks}`
-    );
+        alert(
+          `Employee Details Submitted:\n\n` +
+          `EmpID: ${data.empId}\n` +
+          `Name: ${data.name}\n` +
+          `Designation: ${data.designation}\n` +
+          `Reporting_To: ${data.reportingTo}\n` +
+          `Billable: ${data.billableStatus}\n` +
+          `Skills: ${data.skills}\n` +
+          `Project_Allocation: ${data.projectAllocation}\n` +
+          `Location: ${data.location}\n` +
+          `Email: ${data.email}\n` +
+          `CTE_DOJ: ${data.cteDoj}\n` +
+          `Remarks: ${data.remarks}`
+        );
         this.myform.reset();
       });
     }
@@ -209,22 +220,21 @@ onProjectChange(value: string): void {
       otherCtrl?.setValidators([Validators.required]);
     } else {
       otherCtrl?.clearValidators();
-      otherCtrl?.setValue('');  
+      otherCtrl?.setValue('');
     }
     otherCtrl?.updateValueAndValidity();
   }
 
-resetForm() {
-  this.myform.reset();
-}
-selectedFile: File | null = null;
-uploadFile(event: any) {
-this.selectedFile = event.target.files[0];
-}
+  resetForm() {
+    this.myform.reset();
+  }
 
-// some comment
+  selectedFile: File | null = null;
+  uploadFile(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
 
-submitFile() {
+  submitFile() {
     if (!this.selectedFile) {
       alert('Please select a file.');
       return;
@@ -234,10 +244,8 @@ submitFile() {
       next: () => alert('Upload successful!'),
       error: () => alert('Upload failed.')
     });
-    
 
-    // Good project
-    
+
   }
 
 }
